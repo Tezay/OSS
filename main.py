9920a4f652c237ce611a3d50136384e82b2d7ef2
edouard-torres <edouard.torres05@gmail.com>
2025-02-03 11:06:46 +0100

    monté fusée

--- main.py
+++ main.py
@@ -2,21 +2,89 @@ from idlelib.textview import view_text
 
 import pygame
 import math
+import numpy
+
+pygame.init()
+screen = pygame.display.set_mode((1500, 700))
+pygame.display.set_caption("jeu de la fusée")
 
 
+couleur=(0,0,255)
+position=[750,600]
 
 
-keys = pygame.key.get_pressed()
-
-class fusée():
-    def __init__(self,taille,masse,vitesse_max,vitesse):
+class Fusée():
+    def __init__(self,taille,hauteur,masse,vitesse_max,vitesse,position):
         self.taille = taille
         self.masse = masse
         self.vitesse_max = vitesse_max
         self.vitesse=vitesse
+        self.hauteur = hauteur
+        self.position=position
+
+
+    def touche(self):
+        if keys[pygame.K_SPACE]:
+            print("ici")
+            if self.vitesse<self.vitesse_max:
+                self.vitesse+=1
+            if self.vitesse==self.vitesse_max:
+                self.vitesse=self.vitesse_max
+        else:
+            if self.vitesse!=0:
+                self.vitesse-=1
+
+    def deplacement(self):
+        if self.vitesse>self.vitesse_max/2 and self.vitesse!=self.vitesse_max:
+            self.position[1]+=1
+        elif self.vitesse==self.vitesse_max:
+            self.position[1]+=2
 
 
 
 
+    def dessiner(self,ecran):
+        pygame.draw.rect(ecran,couleur,(self.position[0],self.position[1],self.taille,self.hauteur))
 
-    def dessiner(self):
+
+
+running=True
+
+fusé=Fusée(20,100,20,100,0,position)
+
+
+while running:
+    keys = pygame.key.get_pressed()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
+
+    if keys[pygame.K_SPACE]:
+        print("ici")
+        if fusé.vitesse == fusé.vitesse_max:
+            fusé.vitesse = fusé.vitesse_max
+        else:
+            fusé.vitesse += 1
+
+    else:
+        if fusé.vitesse != 0:
+            fusé.vitesse -= 1
+
+    if fusé.vitesse > fusé.vitesse_max / 2 and fusé.vitesse != fusé.vitesse_max:
+        position[1] += 1
+    elif fusé.vitesse == fusé.vitesse_max:
+        position[1] += 2
+
+    print(fusé.vitesse)
+    print(fusé.position)
+
+    pygame.draw.rect(screen, couleur, (fusé.position[0], fusé.position[1], fusé.taille, fusé.hauteur))
+
+    pygame.display.update()
+    pygame.time.Clock().tick(60)
+
+pygame.quit()
