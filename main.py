edadb7697016da2c341cef9bb19af54dcac80754
edouard-torres <edouard.torres05@gmail.com>
2025-02-03 10:11:33 +0100

    test fusée

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("YEAHHH")
+from idlelib.textview import view_text
+
+import pygame
+import math
+
+
+
+
+keys = pygame.key.get_pressed()
+
+class fusée():
+    def __init__(self,taille,masse,vitesse_max,vitesse):
+        self.taille = taille
+        self.masse = masse
+        self.vitesse_max = vitesse_max
+        self.vitesse=vitesse
+
+
+
+
+
+    def dessiner(self):
